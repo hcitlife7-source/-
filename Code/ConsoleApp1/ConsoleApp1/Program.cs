d7fac30cd139ceab1c19090203d016ed20864423
hcitlife7-source <hcitlife7@gmail.com>
2025-10-24 00:34:04 +0330

    Add files via upload

---
+++ Code/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter number : ");
+        double radius = Convert.ToDouble(Console.ReadLine());
+
+        double circumference = 2 * Math.PI * radius;
+        double area = Math.PI * radius * radius;
+
+        Console.WriteLine("محیط دایره: " + circumference);
+        Console.WriteLine("مساحت دایره: " + area);
+    }
+}
