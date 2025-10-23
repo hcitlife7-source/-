4132b29440c2cb5c48b855b58fc87479390e3fbb
hcitlife7-source <hcitlife7@gmail.com>
2025-10-24 01:20:59 +0330

    Add files via upload

---
+++ Code 2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class program
+{
+    static void Main()
+    {
+        Console.Write("لطفا دما را به سانتی گراد وارد کنید: ");
+        double celsius = Convert.ToDouble(Console.ReadLine());
+
+        double fahrenheit = celsius * 9 / 5 + 32;
+
+        Console.WriteLine("دمای معادل به فارنهایتک: " + fahrenheit); 
+    }
+}
