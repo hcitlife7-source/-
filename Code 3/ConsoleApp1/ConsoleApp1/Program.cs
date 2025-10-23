49246db14d11e6018dba5397fa48afb7e54412b4
hcitlife7-source <hcitlife7@gmail.com>
2025-10-24 02:29:56 +0330

    Add files via upload

---
+++ Code 3/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class program
+{
+    static void Main()
+    {
+        Console.Write("Enter number: ");
+        double number = Convert.ToDouble(Console.ReadLine());
+
+        if (number < 1)
+        {
+            Console.WriteLine("TRUE !");
+        }
+    }
+}
---
+++ Code 4/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class program
+{
+    static void Main()
+    {
+        Console.Write("Enter number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number % 3 == 0 )
+        {
+            Console.WriteLine("Yes !");
+        }
+        else
+        {
+            Console.WriteLine(number * 3);
+        }
+    }
+}
